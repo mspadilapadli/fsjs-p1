f4548e59850e886915e0664700c5125cf837d4bb
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-22 22:34:41 +0700

    calculator.js file

---
+++ week1/fsjs-p1-uc2/advanced-calculator/calculator.js
@@ -0,0 +1,57 @@
+"use strict";
+
+class Calculator {
+    //write your code here
+    constructor() {
+        this.result = 0;
+    }
+    add(num) {
+        this.result += num;
+        return this;
+    }
+    subtract(num) {
+        this.result -= num;
+        return this;
+    }
+    multiply(num) {
+        this.result *= num;
+        return this;
+    }
+    divide(num) {
+        if (num !== 0) {
+            this.result /= num;
+        } else {
+            console.log("ga bisa di bagi 0 kang");
+        }
+        return this;
+    }
+    square() {
+        this.result *= this.result;
+        return this;
+    }
+    squareRoot() {
+        this.result = Math.sqrt(this.result);
+        return this;
+    }
+
+    areaOfTheCircle(r) {
+        this.result = Math.PI * r * r;
+        return this;
+    }
+    circumference(r) {
+        this.result = 2 * Math.PI * r;
+        return this;
+    }
+}
+
+/**
+ * you can use several features from ecmascript, such as:
+ * - Classes
+ * - Default Parameters
+ * - Destructured Assignment
+ * - Template Literals
+ * - Method Chaining
+ */
+
+// execute Calculator in file index.js
+module.exports = Calculator;
