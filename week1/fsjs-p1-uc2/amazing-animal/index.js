ef618f857b86113355018f47ce1de8a63afafa92
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-22 22:31:14 +0700

    amazing_animal part 2

--- week1/fsjs-p1-uc2/amazing-animal/index.js
+++ week1/fsjs-p1-uc2/amazing-animal/index.js
@@ -43,3 +43,75 @@ My favorite foods are ${this.favouriteFoods.join(", ")}`
 // ruruTheCat.move();
 // chalieTheDog.move();
 // nemoTheFish.move();
+
+// ! Amazing Animal Part 2
+
+//*child class Cat
+class Cat extends Animal {
+    //your code here
+    constructor(name, age, type, favouriteFoods, totalLegs) {
+        super(name, age, "Cat", favouriteFoods, totalLegs);
+    }
+}
+const ruruTheCat = new Cat("Ruru", 1, "Cat", ["Salmon", "Tuna", "Chiken"]); // Instantiate = proses pembuatan instance
+
+//*child class Dog
+class Dog extends Animal {
+    //your code here
+    constructor(name, age, type, favouriteFoods, totalLegs) {
+        super(name, age, "Dog", favouriteFoods, totalLegs);
+    }
+}
+const chalieTheDog = new Dog("Charlie", "2", "Dog", ["Meat", "Cake"]);
+
+//*child class Fish
+class Fish extends Animal {
+    //your code here
+    constructor(name, age, type, favouriteFoods, totalLegs, totalFins = 8) {
+        super(name, age, "Fish", favouriteFoods, 0);
+        this.totalFins = totalFins;
+    }
+    move() {
+        console.log("I use my fins to swimming");
+    }
+}
+const nemoTheFish = new Fish("Nemo", 1, "Fish", ["Pellets", "Moss", "Worms"]);
+
+// console.log(ruruTheCat);
+// console.log(nemoTheFish);
+// console.log(`\n`);
+
+// // console.log(nemoTheFish.move());
+// ruruTheCat.move();
+// nemoTheFish.move();
+// console.log(`\n`);
+// ruruTheCat.getInformation("Annyeong haseo~");
+// console.log(`\n`);
+// nemoTheFish.getInformation("Aloha~");
+
+// ==============================================
+//* Agregate Relation
+class SuperPower {
+    constructor(superPower) {
+        this.name = superPower;
+    }
+}
+
+//* class SuperCat ini inheritence dari class Cat (cucunya class Animal)
+
+class SuperCat extends Cat {
+    //your code here
+    constructor(name, age, favouriteFoods, totalLegs, superAbility) {
+        super(name, age, favouriteFoods, totalLegs);
+        this.SuperPower = superAbility;
+    }
+}
+let mechaClaw = new SuperPower("Mecha Claw");
+const felixTheCat = new SuperCat(
+    "Felix",
+    100,
+    ["Chiken Wings", "Pizza"],
+    4,
+    mechaClaw
+);
+console.log(felixTheCat);
