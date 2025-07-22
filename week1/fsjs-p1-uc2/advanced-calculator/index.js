efed4a75a52772e338640b88d3a3c3424115665a
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-22 22:35:03 +0700

    index.js calculator

---
+++ week1/fsjs-p1-uc2/advanced-calculator/index.js
@@ -0,0 +1,17 @@
+"use strict";
+const Calculator = require("./calculator");
+// execute Calculator in here
+const cal = new Calculator();
+// // console.log(cal);
+// let result = cal.add(100).subtract(50).multiply(2).divide(5);
+// console.log(result);
+let result = cal
+    .add(8)
+    .subtract(3)
+    .multiply(2)
+    .divide(2)
+    .square()
+    .squareRoot()
+    .areaOfTheCircle(4)
+    .circumference(2);
+console.log(result);
