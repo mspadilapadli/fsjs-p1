6dc9b998015b7a43640b2089c0709a55b740afb7
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-23 20:50:18 +0700

    amazing_animal part 1 : release 2

--- week1/fsjs-p1-uc2/amazing-animal/index.js
+++ week1/fsjs-p1-uc2/amazing-animal/index.js
@@ -1,28 +1,48 @@
 "use strict";
 //*  Amazing Animal Part 1
 class Animal {
-    constructor(name, age, type, favoriteFoods, totalLegs) {
-        this.name = "Ruru";
-        this.age = 1;
-        this.type = "Cat";
-        this.favoriteFoods = ["Salmon", "Tuna", "Chicken"];
-        this.totalLegs = 4;
+    constructor(name, age, type, favoriteFoods, totalLegs = 4) {
+        this.name = name;
+        this.age = age;
+        this.type = type;
+        this.favoriteFoods = favoriteFoods;
+        this.totalLegs = totalLegs;
     }
     //method here
     move() {
-        console.log("I walking and running with my leg");
+        this.totalLegs
+            ? console.log("I walking and running with my leg")
+            : console.log("I use my fins to swimming");
     }
 
     getInformation(greetings) {
-        console.log(`${this.move} 
-            ${greetings} My name ${this.name}
+        this.move();
+        console.log(`${greetings} My name ${this.name}
             My current age is ${this.age} years old
             I am a ${this.type} 
-            My favorite foods are ${this.favoriteFoods.split(",").join(", ")}`);
+            My favorite foods are ${this.favoriteFoods.join(", ")}`);
     }
 }
 
-const ruruTheCat = new Animal(); //proses instanciateb
-console.log(ruruTheCat); // Instance ruruThecat
-ruruTheCat.name = "Ruru the cat";
-console.log(ruruTheCat);
+// const ruruTheCat = new Animal(); //proses instanciateb
+// console.log(ruruTheCat); // Instance ruruThecat
+
+// ruruTheCat.name = "Ruru the cat";
+// console.log(ruruTheCat);
+
+const charlieTheDog = new Animal("Charli", 2, "Dog", ["Meat", "Cake"]);
+console.log(charlieTheDog);
+
+// charlieTheDog.move();
+charlieTheDog.getInformation("Swadikap~!");
+
+const nemoTheFish = new Animal(
+    "Nemo",
+    1,
+    "Fish",
+    ["Pellete", "Moss", "Worm"],
+    0
+);
+console.log(nemoTheFish);
+
+nemoTheFish.move();
