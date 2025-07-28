aa39b3680e65f8aa0fb9fe83666a7add5e2ea87a
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-28 13:07:55 +0700

    amazing_animal  : release 3 - enhance your animal

--- week1/fsjs-p1-uc2/amazing-animal/index.js
+++ week1/fsjs-p1-uc2/amazing-animal/index.js
@@ -47,20 +47,22 @@ class Animal {
 
 //* Amazing animal part 2
 // inheritance
+// in child class animal don't need type , cuz type difined in super()
 class Cat extends Animal {
     //super() : inheritancer property from parent
-    constructor(name, age, type, favoriteFoods, totalLegs) {
-        super(name, age, "Cat", favoriteFoods, totalLegs);
+
+    constructor(name, age, favoriteFoods) {
+        super(name, age, "Cat", favoriteFoods);
     }
 }
 class Dog extends Animal {
-    constructor(name, age, type, favoriteFoods, totalLegs) {
-        super(name, age, "Dog", favoriteFoods, totalLegs);
+    constructor(name, age, favoriteFoods) {
+        super(name, age, "Dog", favoriteFoods);
     }
 }
 class Fish extends Animal {
-    constructor(name, age, type, favoriteFoods, totalLegs, totalFins = 4) {
-        super(name, age, "Fish", favoriteFoods, 0);
+    constructor(name, age, favoriteFoods, totalLegs = 0, totalFins = 5) {
+        super(name, age, "Fish", favoriteFoods, totalLegs);
         // only fish have property of fins
         this.totalFins = totalFins;
     }
@@ -69,15 +71,66 @@ class Fish extends Animal {
         console.log("I use my fins to swimming");
     }
 }
+class Bird extends Animal {
+    constructor(name, age, favoriteFoods, totalLegs = 2) {
+        super(name, age, "Bird", favoriteFoods, totalLegs);
+    }
 
-const ruruTheCat = new Cat("Ruru", 1, "Cat", ["Salmon", "Tuna", "Chiken"]);
-const charlieTheDog = new Dog("Charli", 2, "Dog", ["Meat", "Cake"]);
-const nemoTheFish = new Fish("Nemo", 1, "Fish", ["Pellete", "Moss", "Worm"], 0);
+    move() {
+        console.log("I fly with my wings and hop with my legs.");
+    }
+}
+const ruruTheCat = new Cat("Ruru", 1, ["Salmon", "Tuna", "Chiken"]);
+const charlieTheDog = new Dog("Charli", 2, ["Meat", "Cake"]);
+const nemoTheFish = new Fish("Nemo", 1, ["Pellete", "Moss", "Worm"], 0);
+const parrot = new Bird("Rio", 3, ["Fruits", "Seeds"]);
 
-console.log(ruruTheCat);
-console.log(charlieTheDog);
-console.log(nemoTheFish);
+// console.log(ruruTheCat);
+// console.log(charlieTheDog);
+// console.log(nemoTheFish);
+// console.log(parrot);
 
-ruruTheCat.move();
-charlieTheDog.move();
-nemoTheFish.move();
+// ruruTheCat.move();
+// charlieTheDog.move();
+// nemoTheFish.move();
+
+class SuperPower {
+    constructor(name) {
+        this.name = name;
+    }
+    punch() {
+        console.log("Hit hit hit ! kitty punch");
+    }
+}
+
+// extends cat (cucu animal)
+class SuperCat extends Cat {
+    // in child class animal don't need type , cuz type difined in super()
+    constructor(name, age, favoriteFoods, superPower) {
+        super(name, age, favoriteFoods);
+        this.superPower = superPower;
+    }
+    // overriding polymorph
+    getInformation(greetings) {
+        this.move();
+        console.log(`${greetings} My name ${this.name}
+            My current age is ${this.age} years old
+            I am a ${this.type} 
+            My favorite foods are ${this.favoriteFoods.join(", ")}
+            
+            I have super power and wanna save the world ! meow~!
+            `);
+    }
+}
+
+const mechaClaw = new SuperPower("Mecha Claw");
+const felixTheCat = new SuperCat(
+    "Felix",
+    100,
+    ["chiken wings", "pizza"],
+    mechaClaw
+);
+
+// console.log(felixTheCat);
+// felixTheCat.getInformation("Ohayooo");
+// felixTheCat.superPower.punch();
