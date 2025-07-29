d5491461c93c13d991357a3f00875cf81371195b
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-29 08:20:58 +0700

    mangoTree : release 7 - create inheritance MangoTree & Mango classes

---
+++ week1/fsjs-p1-c1/mangoTree.js
@@ -0,0 +1,35 @@
+"use strict";
+const { FruitTree, Fruit } = require("./fruitTree");
+
+//* release 7
+class MangoTree extends FruitTree {
+    constructor(deathAge = 10) {
+        super(5, deathAge);
+    }
+}
+
+class Mango extends Fruit {}
+
+const mangoZainudin = new Mango();
+const mangoHanyati = new Mango();
+
+// console.log(mangoZainudin);
+// console.log(mangoHanyati);
+
+//* : driver code
+let mangoTree = new MangoTree(); // menanam pohon
+console.log(mangoTree);
+console.log(mangoTree.deathAge);
+
+console.log(`The tree is alive! :smile:`);
+do {
+    mangoTree.grow();
+    mangoTree.produceFruits();
+    mangoTree.harvest();
+    console.log(
+        `[Year ${mangoTree.age} Report] Height = ${mangoTree.height} m | Fruits harvested = ${mangoTree.harvestReport}`
+    );
+    mangoTree.resetHarvest();
+} while (mangoTree.healthStatus != false);
+
+console.log(`The tree has met its end. :sad:`);
