6bcbf9241548645a9555815e34be8586938abf1c
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-30 09:21:11 +0700

    todo : release 1 - add oneline version code in Model.readData()

--- week1/fsjs-p1-uc3/Models/model.js
+++ week1/fsjs-p1-uc3/Models/model.js
@@ -3,12 +3,20 @@ const Controller = require("../Controllers/controller");
 const Factory = require("./class");
 class Model {
     static readData() {
+        //*step
         // read data with fs and conver from string to array
         // instantaite data
         // return data inctance
-        let data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
-        data = Factory.createTodos(data);
-        return data;
+
+        //* v1
+        //   let data = JSON.parse(fs.readFileSync("./data.json", "utf-8"));
+        //   data = Factory.createTodos(data);
+        //   return data;
+
+        //* v2 one line
+        return Factory.createTodos(
+            JSON.parse(fs.readFileSync("./data.json", "utf-8"))
+        );
     }
 }
 
