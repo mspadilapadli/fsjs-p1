914057df5de79413f3df7a74ea03a34e26f441d8
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-07-31 21:32:48 +0700

    todo : release 1 - costumize factory class

--- week1/fsjs-p1-uc3/Models/class.js
+++ week1/fsjs-p1-uc3/Models/class.js
@@ -11,13 +11,13 @@ class Factory {
         //* v1 : simpan data lalu return
         //   let data = todos.map((todo) => {
         //       let { id, task } = todo;
-        //       return this.createTodo(id, task);
+        //       return this.createTodo(id, task,completed);
         //   });
         //   return data;
         //* v2 : langsung return todos nya
         //   return todos.map((todo) => {
         //       let { id, task } = todo;
-        //       return this.createTodo(id, task);
+        //       return this.createTodo(id, task,completed);
         //   });
         //* v3 : one line
         return todos.map(({ id, task, completed }) =>
