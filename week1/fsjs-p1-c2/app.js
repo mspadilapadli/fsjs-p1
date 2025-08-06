3dfa5c407c1f446a74b3d62e30039a045f6822a1
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-06 09:00:04 +0700

    airport : release 1 - command list

--- week1/fsjs-p1-c2/app.js
+++ week1/fsjs-p1-c2/app.js
@@ -3,9 +3,27 @@ const Controller = require("./Controllers/controller");
 const [command, ...input] = process.argv.slice(2);
 
 switch (command) {
-    case value:
+    case "help":
+        Controller.showCommands();
+        break;
+    case "planeList":
+        Controller.showCommands();
+        break;
+    case "passengerList":
+        Controller.showCommands();
+        break;
+    case "flightInfo":
+        Controller.showCommands();
+        break;
+    case "buyTicket":
+        Controller.showCommands();
+        break;
+    case "ticketInfo":
+        Controller.showCommands();
         break;
 
     default:
+        Controller.showCommands();
+
         break;
 }
