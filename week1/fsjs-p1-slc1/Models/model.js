40ac0428f9dca02f2031c3879c48a720f00b6568
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-08-07 10:02:42 +0700

    slc1-world_bank: release 1 - listBank()

--- week1/fsjs-p1-slc1/Models/model.js
+++ week1/fsjs-p1-slc1/Models/model.js
@@ -1,9 +1,17 @@
+const { join } = require("path");
 const Controller = require("../Controllers/controller");
+const Factory = require("./class");
 const fs = require("fs").promises;
 
 class Model {
     static async getBankList() {
         try {
+            return {
+                code: "listBank",
+                data: Factory.createBanks(
+                    JSON.parse(await fs.readFile("./data.json", "utf-8"))
+                ),
+            };
         } catch (error) {
             throw error;
         }
--- week1/fsjs-p1-slc1/Views/view.js
+++ week1/fsjs-p1-slc1/Views/view.js
@@ -1,2 +1,17 @@
-class View {}
+class View {
+    static showData({ code, data }) {
+        switch (code) {
+            case "listBank":
+                console.log(data);
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    static showError(error) {
+        console.log(error);
+    }
+}
 module.exports = View;
