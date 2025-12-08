e146b1d358fbeea9f23422ce8481d72d8aa37f85
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-12-08 07:01:43 +0700

    continue(early exit), filter() & map()

--- week1/fsjs-p1-uc1/problem-solving/index.js
+++ week1/fsjs-p1-uc1/problem-solving/index.js
@@ -6,20 +6,40 @@ let members = {
     Anton: 4,
     Budi: 5,
 };
-for (let i = startDate; i < endDate; i++) {
-    let date = i;
+// for (let i = startDate; i <= endDate; i++) {
+//     let date = i;
+//     if (date % 5 == 0) {
+//         console.log("Tempat Fitness Tutup");
+//     } else {
+//         let visited = [];
+//         for (const key in members) {
+//             const el = members[key];
+//             if (i == startDate || (i - startDate) % el == 0) {
+//                 visited.push(key);
+//             }
+//         }
+//         console.log(`Tanggal ${date} : ${visited.join(", ")} `);
+//     }
+// }
+
+//* continue, filter() & map()
+
+const memberss = [
+    { name: "Tono", interval: 2 },
+    { name: "Anton", interval: 4 },
+    { name: "Budi", interval: 5 },
+];
+
+for (let date = startDate; date <= endDate; date++) {
     if (date % 5 == 0) {
-        console.log("Tempat Fitness Tutup");
-    } else {
-        let visited = [];
-        for (const key in members) {
-            const el = members[key];
-            if (i == startDate || (i - startDate) % el == 0) {
-                visited.push(key);
-            }
-        }
-        console.log(`Tanggal ${date} : ${visited.join(", ")} `);
+        console.log(`Tanggal ${date}: Tempat Fitness Tutup`);
+        continue; // langsung skip ke tanggal berikutnya
     }
+    const visited = memberss
+        .filter(({ interval }) => (date - startDate) % interval == 0)
+        .map(({ name }) => name);
+
+    console.log(`Tanggal ${date}: ${visited.length ? visited.join(", ") : ""}`);
 }
 
 //!=========================================================================
--- week1/fsjs-p1-uc1/problem-solving/readme.md
+++ week1/fsjs-p1-uc1/problem-solving/readme.md
@@ -4,6 +4,29 @@ Dibebaskan untuk menggunakan type data apa saja, variable, input, built-in funct
 
 Coba buat code yang menggunakan perhitungan, bukan di hardcode.
 
+Learning Competencies
+
+-   Memahami cara untuk menyelesaikan masalah berdasarkan cerita
+-   Gunakan Pseudocode secara efektif untuk memetakan masalah dan menciptakan solusi
+
+Summary
+
+Sebuah tempat fitness ingin memiliki perhitungan kalender jadwal member yang datang dalam bulan tersebut. Setiap member memiliki jadwal yang berbeda-beda:
+
+-   Tono, 2 hari sekali
+-   Anton, 4 hari sekali
+-   Budi, 5 hari sekali
+
+Tempat fitness pertama kali dibuka pada tanggal 7, dan semua member di atas datang pada hari pembukaan. Tempat fitness akan tutup/libur setiap kelipatan tanggal 5. Asumsi bahwa kalender tiap bulan berjumlah 31 hari.
+
+Bantu tempat fitness tersebut untuk mengimplementasikan masalah mereka.
+
+Release 0 : Build from Pseudocode
+
+Buatlah pseudocode berdasarkan requirement di atas, setelah itu buatlah code berdasarkan pseudocode yang sudah dibuat.
+
+Berikut contoh hasil dari kalender yang dibuat
+
 ```txt
 Tanggal 7: Tono, Anton, Budi
 Tanggal 8:
