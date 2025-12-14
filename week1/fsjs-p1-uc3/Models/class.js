0612131020db761fd8d46b8219d7574af4d035d8
mspadilapadli <157938332+mspadilapadli@users.noreply.github.com>
2025-12-14 08:07:10 +0700

    factory method: one liner arrow function createTodos()

--- week1/fsjs-p1-uc3/Models/class.js
+++ week1/fsjs-p1-uc3/Models/class.js
@@ -7,23 +7,25 @@ class Todo {
 }
 
 class Factory {
-    static createTodos(todos) {
-        //* v1 : simpan data lalu return
-        //   let data = todos.map((todo) => {
-        //       let { id, task } = todo;
-        //       return this.createTodo(id, task,completed);
-        //   });
-        //   return data;
-        //* v2 : langsung return todos nya
-        //   return todos.map((todo) => {
-        //       let { id, task } = todo;
-        //       return this.createTodo(id, task,completed);
-        //   });
-        //* v3 : one line
-        return todos.map(({ id, task, completed }) =>
+    // static createTodos(todos) {
+    //     //* v1 : simpan data lalu return
+    //     //   let data = todos.map((todo) => {
+    //     //       let { id, task } = todo;
+    //     //       return this.createTodo(id, task,completed);
+    //     //   });
+    //     //   return data;
+    //     //* v2 : langsung return todos nya
+    //     //   return todos.map((todo) => {
+    //     //       let { id, task } = todo;
+    //     //       return this.createTodo(id, task,completed);
+    //     //   });
+
+    // }
+    //*v3 one liner
+    static createTodos = (todos) =>
+        todos.map(({ id, task, completed }) =>
             this.createTodo(id, task, completed)
         );
-    }
 
     static createTodo(id, task, completed) {
         return new Todo(id, task, completed);
